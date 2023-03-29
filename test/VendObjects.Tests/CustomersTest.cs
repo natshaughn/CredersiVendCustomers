4df18daf89324f59596683f5ad27f0526b59009f
Nathan Shaughnessy <nathan.shaughnessy@roq.co.uk>
2023-03-29 15:10:36 +0100

    More test cases added

--- test/VendObjects.Tests/CustomersTest.cs
+++ test/VendObjects.Tests/CustomersTest.cs
@@ -3,6 +3,7 @@ namespace VendObjects.Tests;
 using VendObjects;
 
 public class CustomersTest
+
 {
     [SetUp]
     public void Setup()
@@ -14,16 +15,14 @@ public class CustomersTest
     {
         Customer customer = new Customer(2, "Jack");
         customer.SetMachines(0);
-
         Assert.That(customer.Category, Is.EqualTo("Lead"));
     }
-    
+
     [Test]
     public void aluminiumTest()
     {
         Customer customer = new Customer(3, "Jack");
         customer.SetMachines(1);
-
         Assert.That(customer.Category, Is.EqualTo("Aluminum"));
     }
 
@@ -32,10 +31,8 @@ public class CustomersTest
     {
         Customer customer = new Customer(4, "Jack");
         Customer customerTwo = new Customer(40, "Jill");
-        
         customer.SetMachines(6);
         customerTwo.SetMachines(19);
-
         Assert.That(customer.Category, Is.EqualTo("Silver"));
         Assert.That(customerTwo.Category, Is.EqualTo("Silver"));
     }
@@ -45,23 +42,19 @@ public class CustomersTest
     {
         Customer customer = new Customer(5, "Jack");
         Customer customerTwo = new Customer(50, "Jill");
-        
         customer.SetMachines(20);
         customerTwo.SetMachines(199);
-
         Assert.That(customer.Category, Is.EqualTo("Gold"));
         Assert.That(customerTwo.Category, Is.EqualTo("Gold"));
     }
 
-    // [Test]
+    [Test]
     public void platinumTest()
     {
         Customer customer = new Customer(6, "Jack");
         Customer customerTwo = new Customer(60, "Jill");
-
         customer.SetMachines(200);
         customerTwo.SetMachines(201);
-
         Assert.That(customer.Category, Is.EqualTo("Platinum"));
         Assert.That(customerTwo.Category, Is.EqualTo("Platinum"));
     }
@@ -71,10 +64,8 @@ public class CustomersTest
     {
         Customer customer = new Customer(7, "Jack");
         Customer customerTwo = new Customer(70, "Jill");
-        
         customer.SetMachines(2);
         customerTwo.SetMachines(5);
-
         Assert.That(customer.Category, Is.EqualTo("Bronze"));
         Assert.That(customerTwo.Category, Is.EqualTo("Bronze"));
     }
@@ -82,19 +73,43 @@ public class CustomersTest
     [Test]
     public void negativeMachinesTest()
     {
-        // try
-        // {
-            Customer customer = new Customer(1, "Jack");
-            customer.SetMachines(-5);
-
-            Assert.That(customer.Machines, Is.EqualTo(-5));
-            
-        //     Assert.Fail();
-        // }
-        // catch (Exception)
-        // {
-        //     Assert.Pass();
-        // }
+        Customer customer = new Customer(1, "Jack");
+        customer.SetMachines(-5);
+        Assert.That(customer.Machines, Is.EqualTo(-5));
     }
 
+    [Test]
+    public void removeAllCustomersTest()
+    {
+        Customers db = new Customers();
+        db.RemoveAll();
+        db.Add(db.Create(71, "Nathan"));
+        db.Add(db.Create(83, "Will"));
+        db.Add(db.Create(92, "Tommy"));
+        Assert.That(db.Count(), Is.EqualTo(3));
+        db.RemoveAll();
+        Assert.That(db.Count(), Is.EqualTo(0));
+    }
+
+    [Test]
+    public void addCustomerFunctionTest()
+    {
+        Customers db = new Customers();
+        db.RemoveAll();
+        db.Add(db.Create(523, "Jack"));
+        db.Add(db.Create(642, "Jill"));
+        db.Add(db.Create(764, "Jebediah"));
+        Assert.That(db.Count(), Is.EqualTo(3));
+    }
+
+    [Test]
+    public void nextIdTest()
+    {
+        Customers db = new Customers();
+        db.RemoveAll();
+        db.Add(db.Create(3, "Frank"));
+        Assert.That(db.NextId(), Is.EqualTo(4));
+    }
 }
+
+
